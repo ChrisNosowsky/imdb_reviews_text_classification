3d43c9903678ae137e1ede5f322a35fadead0a0c
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-08-19 00:42:52 -0400

    Parsing globe word-embeddings, adding model

--- word_embeddings_imdb_reviews.py
+++ word_embeddings_imdb_reviews.py
@@ -2,10 +2,12 @@
 import os
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.models import Embedding, Flatten, Dense
 import numpy as np
 
 
-imbd_dir = 'E:\Racec\Documents\MachineLearning\aclImdb'
+imbd_dir = 'aclImdb'
 train_dir = os.path.join(imbd_dir, 'train')
 
 labels = []
@@ -15,7 +17,7 @@ for label_type in ['neg', 'pos']:
     dir_name = os.path.join(train_dir, label_type)
     for fname in os.listdir(dir_name):
         if fname[-4:] == '.txt':
-            f = open(os.path.join(dir_name, fname))
+            f = open(os.path.join(dir_name, fname), encoding="utf8")
             texts.append(f.read())
             f.close()
             if label_type == 'neg':
@@ -24,20 +26,61 @@ for label_type in ['neg', 'pos']:
                 labels.append(1)
 
 
-maxlen = 100
-training_samples = 200
-validation_samples = 10000
-max_words = 10000
+maxlen = 100                    # Cut off reviews after 100 words
+training_samples = 200          # Trains on 200 samples
+validation_samples = 10000      # Validates on 200 samples
+max_words = 10000               # Top 10,000 words in dataset
 
 tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
 
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
 
+data = pad_sequences(sequences, maxlen=maxlen)
 
+labels = np.asarray(labels)
+print('Shape of data tensor:', data.shape)
+print('Shape of label tensor:', labels.shape)
 
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
 
+x_train = data[:training_samples]
+y_train = labels[:training_samples]
+x_val = data[training_samples:training_samples + validation_samples]
+y_val = labels[training_samples:training_samples + validation_samples]
 
+glove_dir = 'glove.6B'
 
+embeddings_index = {}
+f = open(os.path.join(glove_dir, 'glove.6B.100d.txt'))
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
 
+print('Found %s word vectors.' % len(embeddings_index))
 
+embedding_dim = 100
+embedding_matrix = np.zeros((max_words, embedding_dim))
+for word, i in word_index.items():
+    if i < max_words:
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector
 
+model = Sequential()
+model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
 
+model.layers[0].set_weights([embedding_matrix])
+model.layers[0].trainable = False
